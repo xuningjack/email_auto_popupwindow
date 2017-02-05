864b53f9b6747f01366929b56ceb6c213d00e0ee
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-02-05 17:58:04 +0800

    remove bin gen

--- gen/com/example/android_email_auto_popupwindow/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android_email_auto_popupwindow;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
